17247ff17d32dfcf04d6f575888d4f6a59244d10
El Miri Martin <elmiri.martin@gmail.com>
2026-05-04 14:29:25 +0200

    módosítás

--- src/main/java/com/example/MainSceneController.java
+++ src/main/java/com/example/MainSceneController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 @SuppressWarnings("unused")
 public class MainSceneController {
@@ -19,7 +21,7 @@ public class MainSceneController {
     private ListView<String> list1;
 
     @FXML
-    private Button saveButton;
+    private Button modifyButton;
 
     @FXML
     private Button deleteButton;
@@ -27,24 +29,45 @@ public class MainSceneController {
 
     @FXML
     private void initialize() {
-        addFruitButton.setOnAction(e -> onClickAddFruitButton());
-        saveButton.setOnAction(e -> onClickSaveButton());
-        deleteButton.setOnAction(e -> onClickDeleteButton());
+        initEvent();
         String[] content = {"alma", "körte", "barack", "szilva"};
         list1.getItems().addAll(content);
     }
 
-    private void onClickAddFruitButton() {
-        list1.getItems().add(fruitTextField.getText());
-        fruitTextField.setText("");
+    private void initEvent() {
+        addFruitButton.setOnAction(e -> onClickAddFruitButton());
+        modifyButton.setOnAction(e -> onClickModifyButton());
+        deleteButton.setOnAction(e -> onClickDeleteButton());
+        list1.setOnMouseClicked(e -> onClickList(e));
     }
 
-    private void onClickSaveButton() {
-        fruitTextField.setText("");
+    private void onClickAddFruitButton() {
+        if(!fruitTextField.getText().equals("")) {
+            list1.getItems().add(fruitTextField.getText());
+            fruitTextField.setText("");
+        }
+        
+    }
+
+    private void onClickModifyButton() {
+        if(!fruitTextField.getText().equals("")) {    
+            int index = list1.getSelectionModel().getSelectedIndex();
+            list1.getItems().set(index, fruitTextField.getText());
+            fruitTextField.setText("");
+            list1.setDisable(false);
+        }
     }
 
     private void onClickDeleteButton() {
         list1.getItems().remove(fruitTextField.getText());
         fruitTextField.setText("");
+    }
+
+    private void onClickList(MouseEvent e) {
+        if(e.getClickCount() == 2 && e.getButton().equals(MouseButton.PRIMARY)) {
+            String selected = list1.getSelectionModel().getSelectedItem();
+            fruitTextField.setText(selected);
+            list1.setDisable(true);
+        }
     }
 }
